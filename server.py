6bc683ac9c4f47d02c8e23b11941599648690def
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-04-09 17:34:13 +0530

    First commit

---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+from _thread import *
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server = 'localhost'
+port = 5555
+
+server_ip = socket.gethostbyname(server)
+
+try:
+    s.bind((server, port))
+
+except socket.error as e:
+    print(str(e))
+
+s.listen(2)
+print("Waiting for a connection")
+
+currentId = "0"
+pos = ["0:50,50", "1:100,100"]
+def threaded_client(conn):
+    global currentId, pos
+    conn.send(str.encode(currentId))
+    currentId = "1"
+    reply = ''
